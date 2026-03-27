f4ee2c382c8b11b4e16465f7909fbeb21e5cf8b4
HTSousa0 <henrique.t3les@gmail.com>
2026-03-27 13:11:30 -0300

    ;

--- exercicios.uscs/Aula.006/sistema.js
+++ exercicios.uscs/Aula.006/sistema.js
@@ -1,21 +1,14 @@
-let valorCompra = +prompt('Insira o valor da sua compra:')
-let desconto1 = valorCompra * 0.10
-let desconto2 = valorCompra * 0.20
-let valorFinal = valorCompra - desconto1 && valorCompra - desconto2
-
-if (valorCompra >= 100.00 && valorCompra <= 199.99) {
-    console.log('Desconto de 10% aplicado! R$', + valorFinal)
+let valorCompra = Number(prompt('Insira o valor da sua compra:'));
+let valorFinal;
+if (valorCompra >= 100 && valorCompra <= 199.99) {
+    let desconto = valorCompra * 0.10;
+    valorFinal = valorCompra - desconto;
+    console.log('Desconto de 10% aplicado! R$', valorFinal);
+} else if (valorCompra >= 200) {
+    let desconto = valorCompra * 0.20;
+    valorFinal = valorCompra - desconto;
+    console.log('Desconto de 20% aplicado! R$', valorFinal);
+} else {
+    valorFinal = valorCompra;
+    console.log('Sem descontos aplicados à sua compra! R$', valorFinal);
 }
-else if (valorCompra >= 200.00) {
-    console.log('Desconto de 20% aplicado! R$', + valorFinal)
-
-} else { console.log('Sem descontos aplicados à sua compra!') }
-
-
-
-
-
-
-
-
-
